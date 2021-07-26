286b44b4b51f6e455957d2caaae9359e0eb731c5
Vashist-Agarwalla <vashist1202@gmail.com>
2021-07-26 22:14:26 +0530

    💄 Adding Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,19 @@ class MyApp extends StatelessWidget {
               Text(
                 'Vashist Agarwalla',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FULL STACK DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               ),
