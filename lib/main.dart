c911c8d9668989744e20f25e66072ae71112f496
Vashist-Agarwalla <vashist1202@gmail.com>
2021-07-26 18:43:28 +0530

    🎉 Re-Initialise project

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                // height: 100.0,
-                // width: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              SizedBox(
-                width: 50.0,
-              ),
-              Container(
-                // height: 100.0,
-                // width: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                // height: 100.0,
-                // width: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-            ],
+          child: Column(
+            children: <Widget>[],
           ),
         ),
       ),
